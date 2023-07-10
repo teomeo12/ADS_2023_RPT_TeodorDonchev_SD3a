11f70a6f6b3050278f33007644619f241175a1a9
Teodor Donchev <d00240834@student.dkit.ie>
2023-07-10 20:25:53 +0100

    Push function implemented.

--- main.cpp
+++ main.cpp
@@ -8,12 +8,27 @@ using namespace std;
 int main()
 {
     OrderedArray<int> arr(5, 1);
+    arr.push(6);
+    arr.push(2);
+    arr.push(2);
+    arr.push(4);
+    arr.push(1);
 
 
+    cout<<"The size of the array is: "<<arr.length() << endl;
     arr.print();
 
     arr.clear();
+    cout << "\nThe size of the array after the clear function is: " << arr.length() << endl;
+    arr.print();
+
+    arr.push(2);
+    arr.push(4);
+    arr.push(1);
+
+    arr.push(3);
 
     arr.print();
+    
 
 }
--- orderedArr.h
+++ orderedArr.h
@@ -7,10 +7,10 @@ class OrderedArray
 {
 private:
 
-	T* array; //pointer to the array
+	T* array;		 //pointer to the array
 	int lengthOfArr; //length of the array
-	int maxSize; //capacity of the array
-	int growSize; //integer for the amount the array will grow by
+	int maxSize;	 //capacity of the array
+	int growSize;	 //integer for the amount the array will grow by
 public:
 	//constructor with custom growSize
 	OrderedArray(int size, int grow = 1)
@@ -36,7 +36,7 @@ public:
 			array = nullptr;
 		}
 	}
-	//funcion to print the array
+	//print the array
 	void print()
 	{
 		for (int i = 0; i < lengthOfArr; i++)
@@ -44,6 +44,7 @@ public:
 			cout << array[i] << " ";
 		}
 	}
+	//clear the array
 	void clear()
 	{
 		if (array != nullptr)
@@ -57,4 +58,51 @@ public:
 		array = new T[1];
 	}
 
+	// get the number of elements in the array
+	int length()
+	{
+		return lengthOfArr;
+	}
+
+	//insert an element in to an array
+	void push(const T& newElement) {
+	
+		//if the array is full
+		if (lengthOfArr >= maxSize)
+		{
+			//create a new array with the new size
+			maxSize += growSize;
+			T* newArr = new T[maxSize];
+
+			//copy the elements from the old array 
+			for (int i = 0; i < lengthOfArr; i++)
+			{
+				newArr[i] = array[i];
+			}
+
+			//delete the old array and pointer to the new array
+			delete[] array;
+			array = newArr;
+			
+		}
+		
+		//index to insert the new element
+		int index = 0;
+		for (int i = 0; i < lengthOfArr; i++)
+		{
+			if (newElement > array[i])
+			{
+				index++;
+			}
+		}
+		//shift the elements to the right
+		for (int i = 0; i < lengthOfArr - index; i++)
+		{
+			array[lengthOfArr - i] = array[lengthOfArr - i - 1];
+		}
+		
+		//insert the new element
+		array[index] = newElement;
+		lengthOfArr++;
+	}
 };
