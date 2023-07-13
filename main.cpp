4dc831ff61a937f3b487490a9f5e34521417bece
Teodor Donchev <d00240834@student.dkit.ie>
2023-07-13 22:18:51 +0100

    Ordered Array class refactored. Test project added with tests for the class member functions.

--- main.cpp
+++ main.cpp
@@ -7,28 +7,66 @@ using namespace std;
 
 int main()
 {
-    OrderedArray<int> arr(5, 1);
-    arr.push(6);
-    arr.push(2);
-    arr.push(2);
-    arr.push(4);
-    arr.push(1);
+    try
+    {
+		//Question 1
+		OrderedArray<int> arr(0, 7);
+		arr.push(6);
+		arr.push(2);
+		arr.push(2);
+		arr.push(4);
+		arr.push(13);
 
 
-    cout<<"The size of the array is: "<<arr.length() << endl;
-    arr.print();
+		cout << "The size of the array is: " << arr.length() << endl;
+		arr.print();
 
-    arr.clear();
-    cout << "\nThe size of the array after the clear function is: " << arr.length() << endl;
-    arr.print();
+		arr.clear();
+		cout << "\nThe size of the array after the clear function is: " << arr.length() << endl;
+		arr.print();
 
-    arr.push(2);
-    arr.push(4);
-    arr.push(1);
+		arr.push(2);
+		arr.push(4);
+		arr.push(1);
 
-    arr.push(3);
+		arr.push(3);
 
-    arr.print();
+		arr.print();
+
+		cout << "\narrayDoubles" << endl;
+		OrderedArray<double> arr1(5, 1);
+		arr1.push(6.4);
+		arr1.push(2.3);
+		arr1.push(2.1);
+		arr1.print();
+
+		cout << "\narrayFloat" << endl;
+		OrderedArray<float> arr2(5, 1);
+		arr2.push(3.4f);
+		arr2.push(1.3f);
+		arr2.push(23.1f);
+		arr2.print();
+
+    }
+    catch (invalid_argument e)
+    {
+		cout << e.what() << endl;
+    }
+    catch (int e)
+    {
+		cout << "An int exception occurred" << endl;
+    }
+    catch (double d)
+    {
+    }
+	catch (logic_error){
+		cout << "An LOGIC error occurred" << endl;
+	}
+	catch (...) {
+		cout << "An error occurred" << endl;
+	}
+   
     
+
 
 }
--- orderedArr.h
+++ orderedArr.h
@@ -11,98 +11,118 @@ private:
 	int lengthOfArr; //length of the array
 	int maxSize;	 //capacity of the array
 	int growSize;	 //integer for the amount the array will grow by
+
 public:
-	//constructor with custom growSize
-	OrderedArray(int size, int grow = 1)
-	{
-		array = new T[size];
-		maxSize = size;
-		growSize = grow;
-		lengthOfArr = 0;
-	}
-	//default constructor
-	OrderedArray()
-	{
-		array = new T[1];
-		maxSize = 1;
-		growSize = 1;
-		lengthOfArr = 0;
-	}
-	//destructor
-	~OrderedArray() {
-		if (array != nullptr)
-		{
-			delete[] array;
-			array = nullptr;
-		}
-	}
-	//print the array
-	void print()
-	{
-		for (int i = 0; i < lengthOfArr; i++)
-		{
-			cout << array[i] << " ";
-		}
-	}
-	//clear the array
-	void clear()
-	{
-		if (array != nullptr)
-		{
-			delete[] array;
-			array = nullptr;
-		}
-		lengthOfArr = 0;
-		maxSize = 1;
-		growSize = 1;
-		array = new T[1];
-	}
 
-	// get the number of elements in the array
-	int length()
-	{
-		return lengthOfArr;
-	}
-
-	//insert an element in to an array
-	void push(const T& newElement) {
-	
-		//if the array is full
-		if (lengthOfArr >= maxSize)
-		{
-			//create a new array with the new size
-			maxSize += growSize;
-			T* newArr = new T[maxSize];
-
-			//copy the elements from the old array 
-			for (int i = 0; i < lengthOfArr; i++)
-			{
-				newArr[i] = array[i];
-			}
-
-			//delete the old array and pointer to the new array
-			delete[] array;
-			array = newArr;
-			
-		}
-		
-		//index to insert the new element
-		int index = 0;
-		for (int i = 0; i < lengthOfArr; i++)
-		{
-			if (newElement > array[i])
-			{
-				index++;
-			}
-		}
-		//shift the elements to the right
-		for (int i = 0; i < lengthOfArr - index; i++)
-		{
-			array[lengthOfArr - i] = array[lengthOfArr - i - 1];
-		}
-		
-		//insert the new element
-		array[index] = newElement;
-		lengthOfArr++;
-	}
+	OrderedArray();						  //default constructor
+	OrderedArray(int size, int grow=1); //constructor with custom growSize
+	void push(const T& newElement);       //insert an element in to an array 
+	int length();						  //return the length of the array	
+	void clear();						  //clear the array
+	void print();						  //print the array
+	~OrderedArray(); //destructor
 };
+template<class T>
+OrderedArray<T>::OrderedArray()
+{
+	array = nullptr;
+	lengthOfArr = 0;
+	maxSize = 0;
+	growSize = 0;
+}
+
+template<class T>
+OrderedArray<T>::OrderedArray(int size, int grow)
+{
+	if (size <= 0)
+		throw invalid_argument("The size of the array must be greater than 0");
+	
+	if (grow <= 0)
+		throw logic_error("The grow size of the array must be greater than 0");
+	
+
+	array = new T[size];
+	lengthOfArr = 0;
+	maxSize = size;
+	growSize = grow;
+}
+
+template<class T>
+OrderedArray<T>::~OrderedArray()
+{
+	if (array != nullptr)
+	{
+		delete[] array;
+		array = nullptr;
+	}
+}
+
+template<class T>
+void OrderedArray<T>:: push(const T& newElement) {
+
+	//if the array is full
+	if (lengthOfArr >= maxSize)
+	{
+		//create a new array with the new size
+		maxSize += growSize;
+		T* newArr = new T[maxSize];
+
+		//copy the elements from the old array 
+		for (int i = 0; i < lengthOfArr; i++)
+		{
+			newArr[i] = array[i];
+		}
+
+		//delete the old array and pointer to the new array
+		delete[] array;
+		array = newArr;
+
+	}
+
+	//index to insert the new element
+	int index = 0;
+	for (int i = 0; i < lengthOfArr; i++)
+	{
+		if (newElement > array[i])
+		{
+			index++;
+		}
+	}
+	//shift the elements to the right
+	for (int i = 0; i < lengthOfArr - index; i++)
+	{
+		array[lengthOfArr - i] = array[lengthOfArr - i - 1];
+	}
+
+	//insert the new element
+	array[index] = newElement;
+	lengthOfArr++;
+}
+
+template<class T>
+int OrderedArray<T>::length()
+{
+	return lengthOfArr;
+}
+
+template<class T>
+void OrderedArray<T>::clear()
+{
+	if (array != nullptr)
+	{
+		delete[] array;
+		array = nullptr;
+	}
+	lengthOfArr = 0;
+	maxSize = 0;
+	growSize = 1;
+}
+
+template<class T>
+void OrderedArray<T>::print()
+{
+	for (int i = 0; i < lengthOfArr; i++)
+	{
+		cout << array[i] <<" ";
+	}
+}
